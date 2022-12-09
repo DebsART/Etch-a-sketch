c36725636d98e03247d46a03ed7e11d4a23f4704
Debbie <debby.ek@yahoo.com>
2022-12-09 17:08:00 -0600

    changed for loop, grid now visible

--- java.js
+++ java.js
@@ -4,22 +4,22 @@ let sketchPad = document.getElementById("sketchPad");
 //creates the square
 function divBoxes() {
     let div = document.createElement('div');
-    div.style.width = '16px';
-    div.style.height = '16px';
+    div.classList.add('divBoxes');
+    div.style.width = '40px';
+    div.style.height = '40px';
     div.style.borderColor ='black';
     return div;
 }
 
 // makes square into grid and appends them to html
-function grid(size=16) {
-    for (let i = 0; i <= size ; i++) {
-        for (let x = 0; x<=size; x++) {
-            sketchPad.appendChild(divBoxes(sketchPad.clientWidth / 16))
+function grid() {
+    for (let i = 0; i < 256 ; i++) {
+            sketchPad.appendChild(divBoxes());
         }
     };
-};
 
-console.log(grid(size=16));
+
+console.log(grid())
 
 
 
